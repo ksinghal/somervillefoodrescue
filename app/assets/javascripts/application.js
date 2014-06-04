38330d17aa7d30d0003ecfbb6c8f54f087d40548
Karan Singhal <ksinghal@glasslighttech.com>
2014-06-03 22:42:42 -0400

    updates to volunteer page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,15 @@
 
 $( window ).load(function() {
 	$("embed").remove();
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        $('#file-name').html("<i class='fa fa-camera'></i>&nbsp;&nbsp;"+label);
+    });
+});
+
+$(document)
+    .on('change', '.btn-file :file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
 });
